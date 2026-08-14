00b69ebe7da17cbda1d464fc236b51810b32b0df
Barin A. Moghimi <bamoghimi@gmail.com>
2026-08-14 19:40:39 -0400

    Preliminary 3-state filter

--- include/Simulator.h
+++ include/Simulator.h
@@ -7,38 +7,46 @@
 #include <random>
 #include <vector>
 #include <Eigen/dense>
+
+#include "ICConfig.h"
 #include "ModelConfig.h"
+#include "SimConfig.h"
 
 struct SimulationData {
-    std::vector<Eigen::Vector2d> truth;
-    std::vector<double> measurements;
-    unsigned int N; // length of simulation
+    std::vector<Eigen::Vector3d> truthState;
+    std::vector<double> positionMeasurements;
+    std::vector<double> accelMeasurements;
+    std::vector<double> biasState;
+    std::size_t N; // length of simulation
 };
 
 
 class Simulator {
 public:
-    Simulator(const ModelConfig& config,
-              Eigen::Vector2d x0,
-              double dt,
-              double sigma_z,
-              double simTime,
+    Simulator(const ICConfig& icconfig,
+              const ModelConfig& modelConfig,
+              const SimConfig& simconfig,
               std::mt19937 generator);
 
     SimulationData runSimulator();
+    double accel_true(int i, double dt);
 
 private:
-    double dt_;
-    double sigma_z_;
-    double simTime_;
+    // double dt_;
+    // double sigma_z_;
+    // double simTime_;
+    Eigen::Vector3d x0_;
     std::mt19937 generator_;
 
-    Eigen::Vector2d x0_;
-    Eigen::Matrix2d F_;
-    Eigen::RowVector2d H_;
-    Eigen::Matrix2d Q_;
-    Eigen::Matrix2d L_;
-    double R_;
+    ModelConfig modelConfig_;
+    SimConfig simConfig_;
+    ICConfig icConfig_;
+    // Eigen::Matrix3d F_;
+    // Eigen::RowVector3d H_;
+    // Eigen::RowVector3d B_;
+    // Eigen::Matrix3d Q_;
+    // Eigen::Matrix3d L_;
+    // double R_;
 };
 
 #endif //ESTIMATION_LAB_SIMULATION_H
--- src/Simulator.cpp
+++ src/Simulator.cpp
@@ -4,58 +4,101 @@
 
 #include "Simulator.h"
 
-Simulator::Simulator(const ModelConfig& config,
-                     Eigen::Vector2d x0,
-                     double dt,
-                     double sigma_z,
-                     double simTime,
+Simulator::Simulator(const ICConfig& icConfig,
+                     const ModelConfig& modelConfig,
+                     const SimConfig& simConfig,
                      std::mt19937 generator)
-                     : F_(config.F), H_(config.H), Q_(config.Q), R_(config.R),
-                       x0_(x0), dt_(dt), sigma_z_(sigma_z),
-                       simTime_(simTime), generator_(generator)
+                     : icConfig_(icConfig), modelConfig_(modelConfig),
+                       simConfig_(simConfig), generator_(generator)
 {}
 
 SimulationData Simulator::runSimulator() {
 
     SimulationData data;
 
-    Eigen::LLT<Eigen::Matrix2d> llt(Q_);
-    Eigen::Matrix2d L;
-    if (llt.info() == Eigen::Success) {
-        L = llt.matrixL();
-    } else {
-        throw std::runtime_error("llt.matrixL() failed");
-    }
+    // Extract Model Configuration Parameters
+    double sigma_z = modelConfig_.sigma_z;
+    double sigma_accel = modelConfig_.sigma_accel;
+    double sigma_bias = modelConfig_.sigma_bias;
+    // auto Q = modelConfig_.Q;
+    auto H = modelConfig_.H;
+    auto B = modelConfig_.B;
+    auto F = modelConfig_.F;
 
-    std::normal_distribution<double> std_noise(0.0, 1.0);
-    std::normal_distribution<double> measurement_noise(0.0, sigma_z_);
+    // Extract Simulation Configuration Parameters
+    auto dt = simConfig_.dt;
+    auto simTime = simConfig_.simTime;
 
-    // generate truth + measurements
-    const int N = static_cast<int>(simTime_ / dt_);
+    // Eigen::LLT<Eigen::Matrix3d> llt(Q);
+    // Eigen::Matrix3d L;
+    // if (llt.info() == Eigen::Success) {
+    //     L = llt.matrixL();
+    // } else {
+    //     throw std::runtime_error("llt.matrixL() failed");
+    // }
 
-    std::vector<Eigen::Vector2d> x;
+    // std::normal_distribution<double> std_noise(0.0, 1.0);
+    std::normal_distribution<double> measurement_noise(0.0, sigma_z);
+    std::normal_distribution<double> accel_noise(0.0, sigma_accel);
+    std::normal_distribution<double> bias_noise(0.0, sigma_bias);
+
+    // Generate truth + measurements
+    const int N = static_cast<int>(simTime / dt);
+
+    std::vector<Eigen::Vector3d> x;
     std::vector<double> z;
+    std::vector<double> am;
 
     x.reserve(N);
     z.reserve(N);
+    am.reserve(N);
 
-    auto xt = x0_;
+    // Extract Initial Condition Parameters
+    auto xt = icConfig_.x0;
+
     for (int i = 0; i < N; i++) {
-        Eigen::Vector2d Zeta;
-        Zeta << std_noise(generator_), std_noise(generator_);
+        // Eigen::Vector3d Zeta;
+        // Zeta << std_noise(generator_), std_noise(generator_), std_noise(generator_);
+        // auto w = L * Zeta;
 
-        auto w = L * Zeta;
-        double v = measurement_noise(generator_);
-        z.push_back((H_ * xt)(0) + v);
+        // GENERATE MEASUREMENTS
+        // (position)
+        const double v = measurement_noise(generator_);
+        z.push_back((H * xt)(0) + v);
+        // (acceleration)
+        const double n_a = accel_noise(generator_);
+        double at = accel_true(i, dt);
+        auto b = xt[2];
+        am.push_back(at + b + n_a);
+
+        // GENERATE TRUE STATE
         x.push_back(xt);
-
-        xt = F_ * xt + w;
+        // xt = F_ * xt + w;
+        xt = F * xt + B * at;
+        const double n_b = bias_noise(generator_);
+        xt[2] = xt[2] + n_b;
     };
 
-    data.truth = x;
-    data.measurements = z;
+    data.truthState = x;
+    data.positionMeasurements = z;
+    data.accelMeasurements = am;
     data.N = N;
 
     return data;
-
 }
+
+double Simulator::accel_true(int i, double dt) {
+
+        if (i < static_cast<int>(2.0 / dt)) {
+            return 0.0;
+        } else if (i < static_cast<int>(6.0 / dt)) {
+            return 1.0;
+        } else if (i < static_cast<int>(10.0 / dt)) {
+            return 0.0;
+        } else if (i < static_cast<int>(14.0 / dt)) {
+            return -0.5;
+        } else if (i < static_cast<int>(20.0 / dt)) {
+            return 0.0;
+        } else
+            return 0.0;
+};
